97c59b930c4e4e9b05952c91a7f4c471ab3edf18
polaris1899 <65794985+polaris1899@users.noreply.github.com>
2020-09-08 10:39:33 +0900

    Javasctipt : project filtered

--- main.js
+++ main.js
@@ -54,3 +54,45 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Make the arrow up icon when you scrolls down and click to the Scroll to Top 
+const returnHome = document.querySelector('.return__home');
+
+document.addEventListener('scroll', () => {
+  const currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll >= 5) {
+    returnHome.style.display = "block";
+  } else {
+    returnHome.style.display = "none";
+  }
+});
+  
+returnHome.addEventListener('click',(event) => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+// projects filtered
+const workBtnContainer = document.querySelector('.work__categories')
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if(filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  },300);
+});
+
